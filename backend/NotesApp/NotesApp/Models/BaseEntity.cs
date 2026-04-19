16698da830b3534801592f9c989f57bd4e0a81eb
helviz <twomoelvisrodney@gmail.com>
2026-04-19 12:24:59 +0300

    Renamed `Delete` property to `IsDelete` in `BaseEntity` and set default value to `false`

--- backend/NotesApp/NotesApp/Models/BaseEntity.cs
+++ backend/NotesApp/NotesApp/Models/BaseEntity.cs
@@ -5,5 +5,5 @@ public abstract class BaseEntity
     public int Id { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set;  }
-    public bool Delete { get; set; }
+    public bool IsDelete { get; set; } = false; 
 }
