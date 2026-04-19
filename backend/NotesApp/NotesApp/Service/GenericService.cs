1a8080238cbedb6e17e68e7b8c290553c6e6c710
helviz <twomoelvisrodney@gmail.com>
2026-04-19 21:49:55 +0300

    Added IGenericService interface with CRUD method definitions

---
+++ backend/NotesApp/NotesApp/Service/GenericService.cs
@@ -0,0 +1,13 @@
+using NotesApp.Models;
+
+namespace NotesApp.Service;
+
+public interface IGenericService<T> where T: BaseEntity
+{
+    Task<List<T>> GetAllAync();
+    Task<T> GetById(int id);
+    Task InsertAsync(T entity);
+    Task DeleteById(int id);
+    Task UpdateById(T entity);
+
+}
