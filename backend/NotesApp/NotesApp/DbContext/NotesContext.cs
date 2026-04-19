6c1386f6ccab0b439c3ef9127eb46538c5c48944
helviz <twomoelvisrodney@gmail.com>
2026-04-19 11:14:30 +0300

    Added the DbContext NotesContext

---
+++ backend/NotesApp/NotesApp/DbContext/NotesContext.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using NotesApp.Models;
+
+namespace NotesApp.DbContext;
+
+public class NotesContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Note> Notes { get; set; }
+    public DbSet<NoteBook> NoteBooks { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<Attachment> Attachments { get; set; }
+    
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql(
+            "server=localhost;database=Notesdb;user=root;password=;",
+            ServerVersion.AutoDetect("server=localhost;database=Notesdb;user=root;password=;")
+        );
+
+
+    }
+    
+    
+
+
+}
