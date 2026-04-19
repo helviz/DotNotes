f2c3fdfff02acde16fc87f9d39afdca028eeb0fd
helviz <twomoelvisrodney@gmail.com>
2026-04-19 12:25:54 +0300

    Refactored GenericRepository: updated `GetByIdAsync` and `GetAllAsync` to use expression-bodied syntax, added async `UpdateAsync` and `DeleteAsync` methods, and ensured `AddAsync` persists changes.

--- backend/NotesApp/NotesApp/Repository/Impl/GenericRepository.cs
+++ backend/NotesApp/NotesApp/Repository/Impl/GenericRepository.cs
@@ -1,6 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using NotesApp.Models;
-using NotesApp.DbContext;
+using NotesApp.Data;
 
 namespace NotesApp.Repository.Impl;
 
@@ -13,24 +13,33 @@ public class GenericRepository<T> : IGenericRepository<T> where T: BaseEntity
         _context = context; 
     }
 
-    public async Task<T> GetByIdAsync(int id)
-    {
-        return await _context.Set<T>().FindAsync(id);
-    }
+    public async Task<T?> GetByIdAsync(int id)
+        => await _context.Set<T>().FindAsync(id);
+    
 
     public async Task<List<T>> GetAllAsync()
-    {
-        return await _context.Set<T>().ToListAsync();
-    }
+        => await _context.Set<T>().ToListAsync();
 
     public async Task AddAsync(T entity)
-        => await _context.Set<T>().AddAsync(entity);
+    {
+        await _context.Set<T>().AddAsync(entity);
+        await _context.SaveChangesAsync();
+    }
 
-    public void Update(T entity)
-        => _context.Set<T>().Update(entity);
+    public async Task UpdateAsync(T entity)
+    {
+         _context.Set<T>().Update(entity);
+         await _context.SaveChangesAsync();
+    }
 
-    public void Delete(T entity)
-        => _context.Set<T>().Remove(entity);
-    
+    public async Task DeleteAsync(T entity)
+    {
+        entity.IsDelete = true;
+        _context.Set<T>().Update(entity);
+        await _context.SaveChangesAsync();
+        
+
+    }
+
 
 }
