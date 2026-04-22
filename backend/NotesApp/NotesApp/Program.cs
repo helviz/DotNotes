e77e95abc8ae6ee0885ba94be9e2e12cd415e734
helviz <twomoelvisrodney@gmail.com>
2026-04-22 13:18:56 +0300

    Configured dependency injection for repositories and services in `Program.cs`.

--- backend/NotesApp/NotesApp/Program.cs
+++ backend/NotesApp/NotesApp/Program.cs
@@ -1,12 +1,37 @@
+using NotesApp.Data;
+using NotesApp.Repository;
+using NotesApp.Repository.Impl;
+using NotesApp.Service;
+using NotesApp.Service.Impl;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<NotesContext>();
+
+/*Repository*/
+builder.Services.AddScoped<INoteRepository, NoteRepository>();
+builder.Services.AddScoped<INoteBookRepository, NoteBookRespository>();
+builder.Services.AddScoped<IAttachmentRepository, AttachmentRepository>();
+builder.Services.AddScoped<ITagRepository, TagRepository>();
+builder.Services.AddScoped<IAttachmentRepository, AttachmentRepository>();
+builder.Services.AddScoped<IUserRepository, UserRespository > ();
+
+/*Services*/
+builder.Services.AddScoped<INoteService, NoteService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<INoteBookService, NoteBookService>();
+builder.Services.AddScoped<IAttachmentService, AttachmentService>();
+builder.Services.AddScoped<ITagService, TagService>();
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
