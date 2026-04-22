2165609c1f7b533f203989e585c4127ecc37bf74
helviz <twomoelvisrodney@gmail.com>
2026-04-22 11:28:58 +0300

    Set default values for `IsArchived` and `IsFavorite` properties

--- backend/NotesApp/NotesApp/Models/Note.cs
+++ backend/NotesApp/NotesApp/Models/Note.cs
@@ -6,9 +6,9 @@ public class Note : BaseEntity
     public int NotebookId { get; set; }
     public string Title { get; set; }
     public string Content { get; set; }
-    
-    public bool IsArchived { get; set; }
-    public bool IsFavorite { get; set; }
+
+    public bool IsArchived { get; set; } = false;
+    public bool IsFavorite { get; set; } = false; 
 
     public ICollection<Attachment> Attachments { get;  }= new List<Attachment>();
     public ICollection<Tag> Tags { get;  } = new List<Tag>();
